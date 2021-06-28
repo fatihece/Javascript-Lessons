65e3fa5cc847da84e1af1ad3948c570eed516360
fatih <fatihenaz@gmail.com>
2021-06-28 16:24:34 +0300

    one-line solution added

--- Fizz-Buzz Numbers/app.js
+++ Fizz-Buzz Numbers/app.js
@@ -1,11 +1,19 @@
+//--------------First Solution
+
+// for(let i = 1; i<=100; i++ ){
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log(i, "FizzBuzz");
+//     }else if (i%3 === 0){
+//         console.log(i, "Fizz");
+//     }else if(i%5 === 0){
+//         console.log(i, "Buzz");
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+//----------------Second Solution
+
 for(let i = 1; i<=100; i++ ){
-    if(i%3 === 0 && i%5 === 0){
-        console.log(i, "FizzBuzz");
-    }else if (i%3 === 0){
-        console.log(i, "Fizz");
-    }else if(i%5 === 0){
-        console.log(i, "Buzz");
-    }else{
-        console.log(i);
-    }
+    console.log(`${i} ${i%3 === 0 && "Fizz" || ""}${i%5 ===0 && "Buzz" || ""} `)
 }
