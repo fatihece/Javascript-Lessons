c9f5c99d686c21d2c6a721d1292dc584b4bbcecf
fatih <fatihenaz@gmail.com>
2021-07-29 15:21:21 +0300

    Coding Challange has been created

---
+++ functions/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+///////////////////////////////////////
+// Default Parameters
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+  
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   bookings.push(booking);
+//   console.log(booking);
+// };
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000);
+
+
+
+// if (navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(
+//         function (position) {
+//             const { latitude } = position.coords;
+//             const { longitude } = position.coords;
+//             console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//         },
+//         function () {
+//             alert("Could not get your location")
+//         }
+//     )
